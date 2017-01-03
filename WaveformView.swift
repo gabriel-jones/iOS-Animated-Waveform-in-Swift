0ac4a969cfec7138f418371064e008bd3c1d49ab
Gabriel Jones <gabriel@logic.bm>
2017-01-03 14:31:31 -0400

    Update WaveformView.swift
    Swift 3

--- WaveformView.swift
+++ WaveformView.swift
@@ -2,6 +2,7 @@
 //  WaveformView.swift
 //
 //  Created by Gabriel Jones on 9/22/15.
+//  Updated to Swift 3: 3/1/17
 //  Copyright (c) 2015 Gabriel Jones. All rights reserved.
 //
 
@@ -11,9 +12,9 @@ import AVFoundation
 extension UIColor {
     
     /**
-     Get the RGB of a UIColor as a custom class
+     Get the RGB of a UIColor
      
-     - returns: Dictionary with keys `r`, `g`, `b`, `a` according to UIColor it originated from
+     - returns: `Color` class contains variables `r`, `g`, `b`, `a` according to UIColor it originated from
      */
     func rgb() -> [String:CGFloat] {
         var fRed : CGFloat = 0
@@ -42,12 +43,12 @@ extension UIImage {
      - returns: A UIColor that is the average color in the image
      */
     func averageColor() -> UIColor {
-        let rgba = UnsafeMutablePointer<CUnsignedChar>.alloc(4)
-        let colorSpace: CGColorSpaceRef = CGColorSpaceCreateDeviceRGB()!
-        let info = CGImageAlphaInfo.PremultipliedLast.rawValue
-        let context: CGContextRef = CGBitmapContextCreate(rgba, 1, 1, 8, 4, colorSpace, info)!
+        let rgba = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: 4)
+        let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+        let info = CGImageAlphaInfo.premultipliedLast.rawValue
+        let context: CGContext = CGContext(data: rgba, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: info)!
         
-        CGContextDrawImage(context, CGRectMake(0, 0, 1, 1), self.CGImage)
+        context.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: 1, height: 1))
         
         if rgba[3] > 0 {
             let alpha: CGFloat = CGFloat(rgba[3]) / 255.0
@@ -69,37 +70,45 @@ class WaveformView : UIView {
     var _audioPlayer = AVAudioPlayer()
     var randomColor = false
 
-    func start(inout audioPlayer: AVAudioPlayer) {
-        structure += [1,2,2,2,1]
-        structure += [2,3,4,4,4,3,2,1]
-        structure += [2,3,4,5,6,7,8,8,8,8,8,7,6,5,4,3,2,1]
-        structure += [2,3,4,4,4,3,2,1]
-        structure += [2,3,4,5,6,7,8,9,10,11,12,12,12,12,12,12,12,11,10,9,8,7,6,5,4,3,2,1]
-        structure += [2,3,4,4,4,3,2,1]
-        structure += [2,3,4,5,6,7,8,8,8,8,8,7,6,5,4,3,2,1]
-        structure += [2,3,4,4,4,3,2,1]
-        structure += [2,2,2]
+    func start(_ audioPlayer: inout AVAudioPlayer) {
+        var i = 1
+        var up = true
+        var max = 2
+        var _i = 0
+        let nextmax = [2,4,2,4,8,12,8,2,4,2]
+        while true {
+            if _i == nextmax.count-1 { structure+=[3,2,1,2,2,2,2,2,1];break; }
+            if max == i && up == true {
+                _i += 1
+                structure += [max, max, max, max]
+                max = nextmax[_i]
+                up = false
+            }
+            if i == 1 { up = true }
+            structure.append(i)
+            i += up ? 1 : -1
+        }
 
-        let dpLink = CADisplayLink(target: self, selector: Selector("update"))
+        let dpLink = CADisplayLink(target: self, selector: #selector(WaveformView.update))
         dpLink.frameInterval = 2
-        audioPlayer.meteringEnabled = true
-        dpLink.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSRunLoopCommonModes)
+        audioPlayer.isMeteringEnabled = true
+        dpLink.add(to: RunLoop.current, forMode: RunLoopMode.commonModes)
         _audioPlayer = audioPlayer
     }
     
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
-        CGContextBeginPath(context)
+        context?.beginPath()
         
         var i = CGFloat(0.0)
         for line in structure {
             let _power = power * Float(line)
-            CGContextMoveToPoint(context, CGFloat(1+i), CGFloat(0))
-            let rand = Float(arc4random_uniform(3))
-            let _rand = Float(arc4random_uniform(3)) - 3
+            context?.move(to: CGPoint(x: CGFloat(1+i), y: CGFloat(0)))
+            let rand = Float(arc4random_uniform(2))
+            let _rand = Float(arc4random_uniform(2)) - 2
             let decision = Float(arc4random_uniform(1))
             let final: Float = (decision == 0) ? (rand) : (_rand)
-            CGContextAddLineToPoint(context, CGFloat(1+i), CGFloat(final + (_power * 0.35)))
+            context?.addLine(to: CGPoint(x: CGFloat(1+i), y: CGFloat(final + (_power * 0.35))))
             i += self.frame.width / CGFloat(structure.count)
         }
         
@@ -115,20 +124,21 @@ class WaveformView : UIView {
             a = self.color.rgb()["a"]!
         }
         
-        CGContextSetRGBStrokeColor(context, r, g, b, a)
-        CGContextSetLineWidth(context, 1)
-        CGContextStrokePath(context)
+        context?.setStrokeColor(red: r, green: g, blue: b, alpha: a)
+        context?.setLineWidth(1)
+        context?.strokePath()
     }
-    
+
     func update() {
         //Get new meter values
         _audioPlayer.updateMeters()
         
+        
         var _power = Float()
         var __power = [Float]()
         
-        for var i = 0; i < _audioPlayer.numberOfChannels; i++ {
-             __power.append(_audioPlayer.averagePowerForChannel(i))
+        for i in 0 ..< _audioPlayer.numberOfChannels {
+             __power.append(_audioPlayer.averagePower(forChannel: i))
         }
         _power = __power.reduce(0.0) {
             return $0 + $1/Float(__power.count)
